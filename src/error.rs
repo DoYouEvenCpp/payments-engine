5c75503a50ed19f7e6aa6efad7623c83f144ab92
Stanislaw Kubiak <stahok@gmail.com>
2025-02-28 00:15:18 +0100

    Introduce two new error types
    Add MissingAmount
    Add ResolveOnNonDisputeOperation

--- src/error.rs
+++ src/error.rs
@@ -6,12 +6,16 @@ use crate::record::OperationType;
 pub enum Errors {
     #[error("Account {0} is locked")]
     AccountLocked(u16),
-    #[error("Not enough funds available for account {0}!")]
+    #[error("Not enough funds available for account {0}")]
     InsuficientFunds(u16),
     #[error("Overflow occured in account {0}")]
     FundsOverflow(u16),
+    #[error("Missing amount for the operation")]
+    MissingAmount,
     #[error("Negative amount")]
     NegativeAmount,
-    #[error("Transaction ID {0} already taken in operation {1}!")]
+    #[error("Transaction ID {0} already taken in operation {1}")]
     TransactionIdAlreadyUsed(u32, OperationType),
+    #[error("Resolve requested to a non dispute operation")]
+    ResolveOnNonDisputeOperation,
 }
